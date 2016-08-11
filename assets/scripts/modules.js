25899d0e116329980c6dc532728f589edb66f97f
Quentin Hocdé <quentinhocde@gmail.com>
2016-08-11 16:37:17 -0400

    delete Button and Title module

--- assets/scripts/modules.js
+++ assets/scripts/modules.js
@@ -1,3 +1,2 @@
 /* jshint esnext: true */
-export {default as Button} from './modules/Button';
-export {default as Title} from './modules/Title';
+// export {default as Button} from './modules/Button';
